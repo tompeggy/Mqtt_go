14c3ed4e15c80e6aa518394d11676dd2c050351a
tompeggy <35727101+tompeggy@users.noreply.github.com>
2018-09-26 02:54:43 -0400

    Update mqtt_0926.go

--- mqtt_0926.go
+++ mqtt_0926.go
@@ -1,2 +1,257 @@
+/***********************************************************************************
+PROJECT NAME  Mqtt Client...
+DESCRIPTION   Mqtt Client封包處埋.....
+HardWare      ***
+Copyright   : 2018 TECO Ltd.
+All Rights Reserved
+//influxdb使用...
+influx
+> create user teco with password 'teco1133'
+> grant all privileges to teco  //授權全部的資料庫(等於管理者帳號)...
+
+
+***********************************************************************************/
+/***********************************************************************************
+Revision History
+DD.MM.YYYY OSO-UID Description..
+30.08.2018 Thomas start work..
+25.09.2018 Thomas 修改成NB-IoT方案,import github用指令 go get 加入libery...
+***********************************************************************************/
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/influxdata/influxdb/client/v2"
+
+	"time"
+	//import the Paho Go MQTT library
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
 //
-//
+const (
+	MyDB     = "chiler"
+	username = "teco"
+	password = "teco1133"
+
+//	MyMeasurement = "cpu_usage"
+)
+
+//設定MQTT接收的Buffer...
+const (
+	MaxClientIdLen = 10
+)
+
+var Knt int
+var Rxdata [100]uint8
+
+// var outgoing chan *MQTTMessage
+
+//define a function for the default message handler
+//set callback function
+var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
+	//	mQTTMessage := &MQTTMessage{msg, m}
+	//	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	var indexMessage int
+	Knt++
+	if Knt > 65530 {
+		Knt = 0
+	}
+	fmt.Printf("MSG: %d\n", Knt)
+	//fmt.Printf("%s\n", msg.Payload())
+
+	//轉換資料...
+	s := string(msg.Payload()[:])
+	//	fmt.Printf("%s\n", s)
+	//fmt.Println(strings.Contains(s, "data"))
+	//比較字串,並接收資料...
+	//if strings.Contains(s, "0000000005010e9e") {
+	if strings.Contains(s, "013157800087586") {
+		indexMessage = strings.Index(s, "data")
+		fmt.Printf("index %d\n", indexMessage)
+		//for i := 0; i < 58; i++ {
+		for i := 0; i < 68; i++ {
+			Rxdata[i] = s[indexMessage+i+7]
+		}
+		fmt.Printf("Rxdata: %s\n", Rxdata)
+		//寫入influxdb...
+
+	}
+
+	/*
+		//	fmt.Printf("check: %t\n", (s == "data"))
+		if s == "on" {
+			fmt.Println("on is received!")
+			//	TurnAllOn()
+		} else if s == "off" {
+			fmt.Println("off is received!")
+			//	GlowOff()
+		}
+	*/
+	//	fmt.Println(strings.Contains(msg.Payload(), "data")) //true
+}
+
+/*
+var msgRcvd := func(client *mqtt.Client, message mqtt.Message) {
+	fmt.Printf("Received message on topic: %s\nMessage: %s\n", message.Topic(), message.Payload())
+}
+*/
+
+// getRandomClientId returns randomized ClientId.
+func getRandomClientId() string {
+	const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	var bytes = make([]byte, MaxClientIdLen)
+	rand.Read(bytes)
+	for i, b := range bytes {
+		bytes[i] = alphanum[b%byte(len(alphanum))]
+	}
+	return "ClientID-" + string(bytes)
+}
+
+//influx tets
+/*
+func connInflux() client.Client {
+	cli, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr:     "http://localhost:8086",
+		Username: username,
+		Password: password,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("influx pass\n")
+	return cli
+
+}
+
+*/
+func main() {
+	Knt = 0
+
+	//influxdb測試...
+	// Create a new HTTPClient
+	c, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr:     "http://localhost:8086",
+		Username: username,
+		Password: password,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	fmt.Printf("pass http\n")
+
+	// Create a new point batch
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Database:  MyDB,
+		Precision: "s",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("pass creat db\n")
+
+	// Create a point and add to batch
+	tags := map[string]string{"location": "bank"}
+	fields := map[string]interface{}{
+		"water_temp_in":      27.7,
+		"water_temp_out":     26.6,
+		"ice_temp_in":        14.6,
+		"ice_temp_out":       16.6,
+		"left_temp_out":      33.6,
+		"right_temp_out":     54.6,
+		"left_pressor_high":  4.16,
+		"left_pressor_low":   4.16,
+		"right_pressor_high": 7.75,
+		"right_pressor_low":  2.8,
+		"voltage":            380.0,
+		"left_amp":           0.0,
+		"right_amp":          80.6,
+	}
+
+	pt, err := client.NewPoint("MAC_013157800087586", tags, fields, time.Now())
+	if err != nil {
+		log.Fatal(err)
+	}
+	bp.AddPoint(pt)
+
+	// Write the batch
+	if err := c.Write(bp); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("pass write\n")
+	// Close client resources
+	if err := c.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	//建立getRandomClientId returns randomized ClientId.
+	clientId := getRandomClientId()
+	fmt.Printf("clientId: %s\n", clientId)
+	//建立Go的監聴封包...
+	sigc := make(chan os.Signal, 1)
+
+	signal.Notify(sigc, os.Interrupt, syscall.SIGTERM)
+
+	//create a ClientOptions struct setting the broker address, clientid, turn
+	//off trace output and set the default message handler
+	opts := MQTT.NewClientOptions().AddBroker("tcp://140.124.182.66:1883")
+	//opts := MQTT.NewClientOptions().AddBroker("tcp://13.114.3.126:1883")
+
+	//opts.SetClientID("u89-0001")
+	opts.SetClientID(clientId)
+
+	opts.SetDefaultPublishHandler(f)
+	//
+
+	//topic := "GIOT-GW/UL/1C497BE1FD99"
+	//topic := "iaq"
+	topic := "NB-IoT"
+	//create and start a client using the above ClientOptions
+	client := MQTT.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	} else {
+		fmt.Printf("Connected to server\n")
+	}
+
+	//subscribe to the topic "iaq" and request messages to be delivered
+	//at a maximum qos of zero, wait for the receipt to confirm the subscription
+	if token := client.Subscribe(topic, 0, nil); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		//	fmt.Printf("Received message on topic: %s\nMessage: %s\n", message.Topic(), message.Payload())
+		token.Wait()
+		//	os.Exit(1)
+	}
+
+	//Publish 5 messages to /go-mqtt/sample at qos 1 and wait for the receipt
+	//from the server after sending each message
+	for i := 0; i < 1; i++ {
+		text := fmt.Sprintf("this is msg #%d!\n", i)
+		token := client.Publish(topic, 0, false, text)
+		token.Wait()
+	}
+
+	time.Sleep(5 * time.Second)
+	//unsubscribe from /go-mqtt/sample
+	/*
+		if token := c.Unsubscribe("GIOT-GW/UL/1C497BE1FD99"); token.Wait() && token.Error() != nil {
+			fmt.Println(token.Error())
+			os.Exit(1)
+		}
+	*/
+
+	// Wait for receiving a signal.
+	//<-sigc
+	s := <-sigc
+	fmt.Println("Got signal:", s) //Got signal: terminated
+	//	c.Disconnect(11250)
+}
